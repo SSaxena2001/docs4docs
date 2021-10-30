2200712631041df20c6d562444b8d303285029db
Suvigya Saxena <suvigya2001@gmail.com>
2021-10-30 13:14:31 +0530

    Added the Join component

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
 
 
