bd77aab8ccce21b97da3120f9c1b021c64765849
Suvigya Saxena <suvigya2001@gmail.com>
2021-10-31 07:05:30 +0530

    Chat component fixed

--- index.js
+++ index.js
@@ -116,6 +116,24 @@ io.on('connection', socket => {
   })
 })
 
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  
+  socket.on('joining msg', (username) => {
+  	name = username;
+  	io.emit('chat message', `---${name} joined the chat---`);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('chat message', `---${name} left the chat---`);
+    
+  });
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);         //sending message to all except the sender
+  });
+});
+
 app.get("/wait",(req,res)=>{
   res.render("wait")
 })
